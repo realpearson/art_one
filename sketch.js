5db701b85879304a7158fb14931afe5fdffaeb5d
realpearson <60543594+realpearson@users.noreply.github.com>
2022-06-22 21:41:17 +0200

    pointy

--- sketch.js
+++ sketch.js
@@ -45,24 +45,24 @@ function draw() {
   
   for(let i = 4; i < iterations; i++){
     pg.strokeWeight(noise(pointBuffer[iterations-1][i].offset)*40);
-    //pg.point(startX + iterations * spacing * (strokeSize*0.7), startY + i * spacing * (strokeSize*0.7));
-    //pg.point(startX + i * spacing * (strokeSize*0.7), startY + iterations * spacing * (strokeSize*0.7));
+    pg.point(startX + iterations * spacing * (strokeSize*0.7), startY + i * spacing * (strokeSize*0.7));
+    pg.point(startX + i * spacing * (strokeSize*0.7), startY + iterations * spacing * (strokeSize*0.7));
     
-    pg.line(startX + iterations * spacing * (strokeSize*0.7), startY + i * spacing * (strokeSize*0.7), startX +20 + iterations * spacing * (strokeSize*0.7), startY + 20 + i * spacing * (strokeSize*0.7));
-    pg.line(startX + i * spacing * (strokeSize*0.7), startY + iterations * spacing * (strokeSize*0.7), startX +20 + i * spacing * (strokeSize*0.7), startY + 20 + iterations * spacing * (strokeSize*0.7));
+    //pg.line(startX + iterations * spacing * (strokeSize*0.7), startY + i * spacing * (strokeSize*0.7), startX +20 + iterations * spacing * (strokeSize*0.7), startY + 20 + i * spacing * (strokeSize*0.7));
+    //pg.line(startX + i * spacing * (strokeSize*0.7), startY + iterations * spacing * (strokeSize*0.7), startX +20 + i * spacing * (strokeSize*0.7), startY + 20 + iterations * spacing * (strokeSize*0.7));
   }
   
   for(let x = 0; x < iterations; x++){
     for(let y = 0; y < iterations; y++){
       pg.stroke(noise(pointBuffer[x][y].offset)*255, 30, 48);
       pg.strokeWeight(noise(pointBuffer[x][y].offset)*40); //20
-      pointBuffer[x][y].offset += 0.01; //0.05
+      pointBuffer[x][y].offset += 0.05; //0.05
       
-      //pg.point(startX + x * spacing * (strokeSize*0.7), startY + y * spacing * (strokeSize*0.7));
+      pg.point(startX + x * spacing * (strokeSize*0.7), startY + y * spacing * (strokeSize*0.7));
       
       //pg.line(startX + x * spacing * (strokeSize*0.7), startY + y * spacing * (strokeSize), startX +2 + x * spacing * (strokeSize), startY + 2 + y * spacing * (strokeSize*0.7));
 
-      pg.line(startX + x * spacing * (strokeSize*0.7), startY + y * spacing * (strokeSize*0.7), startX +2 + x * spacing * (strokeSize*0.7), startY + 2 + y * spacing * (strokeSize*0.7));
+      //pg.line(startX + x * spacing * (strokeSize*0.7), startY + y * spacing * (strokeSize*0.7), startX +2 + x * spacing * (strokeSize*0.7), startY + 2 + y * spacing * (strokeSize*0.7));
     }
   }
   pg.filter(BLUR, 0.9 + noise(pointBuffer[0][0].offset)*2);
